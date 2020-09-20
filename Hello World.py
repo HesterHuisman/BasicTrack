3b368c6969dc110db0588674f27c5572f1199251
HesterHuisman <71075786+HesterHuisman@users.noreply.github.com>
2020-09-20 15:02:59 +0200

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,25 @@
+# Week 2
+# Hester Huisman
+
+# 2.1
+a = "All"
+b = " work"
+c = " and"
+d = " no"
+print(a+b+c+d)
+
+# 2.2
+print(6*(1-2))
+
+# 2.4
+bruce = 6
+print(bruce + 4)
+
+# 2.5
+P = float(input("Please enter the principle amount: "))
+r = float(input("Please enter the annual nominal interest rate: "))
+n = float(input("Please enter the number of times the interest is compounded per year: "))
+t = float(input("Please enter the number of years: "))
+
+A = (P(1+r/n))**(n*t)
+
