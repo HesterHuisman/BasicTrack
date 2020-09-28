7b16ba215388f329cae6ca177731a43a080ec13d
HesterHuisman <71075786+HesterHuisman@users.noreply.github.com>
2020-09-28 23:00:24 +0200

    assignments week 3 mandala

--- Week 3/Mandala.py
+++ Week 3/Mandala.py
@@ -1,19 +1,45 @@
 import turtle
 
 paper = turtle.Screen()
-leonardo = turtle.Turtle()
+donut = turtle.Turtle()
 
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+colors = ['yellow', 'hotpink', 'hotpink', 'hotpink', 'yellow', 'yellow']
 
-for i in range(5):
-    leonardo.left(10)
-    leonardo.penup()
-    leonardo.forward(10)
-    leonardo.pendown()
+donut.pensize(3)
+donut.speed(100000)
+
+# make a pink donut
+for i in range(36):
+    donut.left(10)
+    donut.penup()
+    donut.forward(10)
+    donut.pendown()
     for element in range(12):
-        leonardo.color(colors[element % len(colors)])
-        leonardo.forward(50)
-        leonardo.left(30)
+        donut.color(colors[element % len(colors)])
+        donut.forward(50)
+        donut.left(30)
         print(element + 5)
+
+
+donut.penup()
+donut.left(90)
+donut.forward(60)
+donut.left(90)
+donut.forward(5)
+
+donut.speed(1)
+
+# add chocolate
+donut.shape("square")
+donut.shapesize(0.25, 1)
+donut.color('brown')
+
+for i in range(12):
+    donut.right(30)
+    donut.forward(100)
+    donut.stamp()
+    donut.backward(100)
+
+donut.undo()
 
 paper.exitonclick()
