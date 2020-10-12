1c0ee0c3d95d868bacb8829df21753de32cb7283
HesterHuisman <71075786+HesterHuisman@users.noreply.github.com>
2020-10-12 21:48:03 +0200

    Finish 5.1.19

--- Week 5/5.1.19/Assignment 13.py
+++ Week 5/5.1.19/Assignment 13.py
@@ -2,12 +2,16 @@ def remove(part, word):
     """remove part from word"""
     n = word.find(part)
     m = len(part)
-    if part in word:
+    new_word = ""
+    while True:
+        part in word
         part1 = word[:n]
-        part2 = word[(m+1):]
+        part2 = word[m+1:]
         new_word = part1 + part2
-    else:
-        new_word = word
+        word = new_word
+        if part not in word:
+            break
+    new_word = word
     return new_word
 
 
@@ -15,3 +19,4 @@ print(remove("an", "banana"))
 print(remove("cyc", "bicycle"))
 print(remove("iss", "Mississippi"))
 print(remove("eggs", "bicycle"))
+# last one is not working :(
